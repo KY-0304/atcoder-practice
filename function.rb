56cda1cf3c32223f2f2c2412921c19044c23ff41
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-10-31 05:53:46 +0900

    frst commit

---
+++ function.rb
@@ -0,0 +1,9 @@
+def gc() gets.chomp end
+def gci() gc.to_i end
+def gcs() gc.split end
+def gcsmi() gcs.map(&:to_i) end
+def rc() readlines(chomp: true) end
+def rcms() rc.map(&:split) end
+def rcmi() rc.map(&:to_i) end
+
+#---------------------------------------------------
