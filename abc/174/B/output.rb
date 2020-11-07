4e1e849b98275e0f37d42b1343cbb2bd996f75e5
Kyohei Yamada <kyouhei.hey.1994@gmail.com>
2020-11-07 11:10:34 +0900

    abc 174 A,Bの練習

---
+++ abc/174/B/output.rb
@@ -0,0 +1,13 @@
+def gc() gets.chomp end
+def gci() gc.to_i end
+def gcs() gc.split end
+def gcsmi() gcs.map(&:to_i) end
+def rc() readlines(chomp: true) end
+def rcms() rc.map(&:split) end
+def rcmi() rc.map(&:to_i) end
+def rcmsmi() rc.map { |n| n.split.map(&:to_i) } end
+#---------------------------------------------------
+n, d = gcsmi
+points = rcmsmi
+D = d * d
+puts points.count { |x, y| x*x + y*y <= D }
